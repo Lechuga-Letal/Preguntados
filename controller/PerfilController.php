e2171b2d13888a08f1c90ff12a24d462183fce38
JoaquinDiaz <joaquindiazantunez15@gmail.com>
2025-11-24 13:35:25 -0300

    Perfil niveles

--- controller/PerfilController.php
+++ controller/PerfilController.php
@@ -6,13 +6,14 @@ class PerfilController
     private $renderer;
     private $redirectModel;
     private $usuarioModel;
-
-    public function __construct($model, $renderer, $redirectModel, $usuarioModel)
+    private $categoriasModel; 
+    public function __construct($model, $renderer, $redirectModel, $usuarioModel, $categoriasModel)
     {
         $this->model = $model;
         $this->renderer = $renderer;
         $this->redirectModel = $redirectModel;
         $this->usuarioModel = $usuarioModel;
+        $this->categoriasModel = $categoriasModel; 
     }
 
     public function base()
@@ -28,13 +29,25 @@ class PerfilController
         $foto = $_SESSION['foto_perfil'] ?? '/public/imagenes/usuarioImagenDefault.png';
         $esEditor= $this->usuarioEsEditor($usuarioSesion);
         $idSesion = $_SESSION["id"];
+
+        $categoriasYNivelDeUsuario = $this->categoriasModel->getAllCategorias(); 
+
+        foreach($categoriasYNivelDeUsuario as &$cat) {
+            $cat['nivel'] = $this->usuarioModel->getNivelUsuarioPorCategoria($idPerfil, $cat['id_categoria']);
+        }
+
+        $nivelGeneral = $this->usuarioModel->getNivelUsuarioGeneral($idPerfil); 
+
         $data = [
             "usuario" => $usuario,
             "usuarioSesion" => $usuarioSesion,
             "id" => $idSesion,
             "idPerfil" => $idPerfil,
             "foto_perfil" => $foto,
-            "esEditor" => $esEditor
+            "esEditor" => $esEditor,
+
+            "nivelPorCategoria" => $categoriasYNivelDeUsuario,
+            "nivelGeneral" => $nivelGeneral
         ];
 
         $this->renderer->render("perfil", $data);
